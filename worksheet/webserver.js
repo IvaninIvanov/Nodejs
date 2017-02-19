94d397b0d65b9199fe63c7c8b284ff8f3ea86730
Ivanin Ivanov <up778885@myport.ac.uk>
2017-02-19 15:06:59 +0000

    Add files via upload

--- worksheet/webserver.js
+++ worksheet/webserver.js
@@ -4,13 +4,8 @@ var server = http.createServer(
     function (request, response) {
         var parsedUrl = url.parse(request.url, true);
 		    if (parsedUrl.pathname == '/add') {
-            // todo: find out why it does not parse as Int
-            var a = parseFloat(parsedUrl.query.a);
-            var b = parseFloat(parsedUrl.query.b);
-            var sum = a+b;
-
-            response.setHeader("Content-Type", "int");
-            response.end(sum);
+            response.setHeader("Content-Type", "text/plain");
+            response.end(String(Number(parsedUrl.query.a) + Number(parsedUrl.query.b)));
         } else {
             response.statusCode = 404;
             response.end('Not found!\n');
